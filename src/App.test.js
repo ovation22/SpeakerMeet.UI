cdab71a3d2d94192accee42804ce2675f8df43f4
ovation22 <john@1north.com>
2020-07-07 17:40:04 -0400

    Skip test for now

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,9 @@
-/*
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hooray!', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hooray!/i);
   expect(linkElement).toBeInTheDocument();
 });
-*/
