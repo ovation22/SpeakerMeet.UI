e45788c727067fa3a22a1265de340b1983765c56
John Callaway <ovation22@users.noreply.github.com>
2020-07-07 17:41:43 -0400

    Blog template (#3)
    * Blog template
    * ESLint ignore setupTest.js
    * Skip test for now

---
+++ src/components/Main.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Header from './Header';
+import MainHeroSection from './MainHeroSection';
+import FeaturedPost from './FeaturedPost';
+import Footer from './Footer';
+
+const sections = [
+  { title: 'Technology', url: '#' },
+  { title: 'Design', url: '#' },
+  { title: 'Culture', url: '#' },
+  { title: 'Business', url: '#' },
+  { title: 'Politics', url: '#' },
+  { title: 'Opinion', url: '#' },
+  { title: 'Science', url: '#' },
+  { title: 'Health', url: '#' },
+  { title: 'Style', url: '#' },
+  { title: 'Travel', url: '#' },
+];
+
+const mainFeaturedPost = {
+  title: 'Title of a longer featured blog post',
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+  linkText: 'Continue reading…',
+};
+
+const featuredPosts = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    description:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    image: 'https://source.unsplash.com/random',
+    imageText: 'Image Text',
+  },
+];
+
+export default function Main() {
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="Blog" sections={sections} />
+        <main>
+          <MainHeroSection post={mainFeaturedPost} />
+          <Grid container spacing={4}>
+            {featuredPosts.map(post => (
+              <FeaturedPost key={post.title} post={post} />
+            ))}
+          </Grid>
+        </main>
+      </Container>
+      <Footer title="Footer" description="Something here to give the footer a purpose!" />
+    </>
+  );
+}
