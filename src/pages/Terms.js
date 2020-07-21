e13dfe4a46182915f72e04c0390372fe2e2b37d1
John Callaway <ovation22@users.noreply.github.com>
2020-07-21 17:39:32 -0400

    Cleanup (#33)
    * Cleanup
    
    * Cleanup
    
    * env

--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -139,99 +139,93 @@ export default function Terms() {
             except those that are specifically endorsed or approved by us.
           </Typography>
           <Typography paragraph>As a user of the Site, you agree not to:</Typography>
-          <Typography paragraph>
-            <ol>
-              <li>
-                Systematically retrieve data or other content from the Site to create or compile,
-                directly or indirectly, a collection, compilation, database, or directory without
-                written permission from us.
-              </li>
-              <li>
-                Trick, defraud, or mislead us and other users, especially in any attempt to learn
-                sensitive account information such as user passwords.
-              </li>
-              <li>
-                Circumvent, disable, or otherwise interfere with security-related Site, including
-                features that prevent or restrict the use or copying of any Content or enforce
-                limitations on the use of the Site and/or the Content contained therein.
-              </li>
-              <li>Disparage, tarnish, or otherwise harm, in our opinion, us and/or the Site.</li>
-              <li>
-                Use any information obtained from the Site in order to harass, abuse, or harm
-                person.
-              </li>
-              <li>
-                Make improper use of our support services or submit false reports of abuse or abuse
-                or misconduct.
-              </li>
-              <li>
-                Use the Site in a manner inconsistent with any applicable laws or regulations.
-              </li>
-              <li>Use the Site to advertise or offer to sell goods and services.</li>
-              <li>Engage in unauthorized framing of or linking to the Site.</li>
-              <li>
-                Upload or transmit (or attempt to upload or to transmit) viruses, Trojan horses, or
-                other material, including excessive use of capital letters and spamming posting
-                posting of repetitive text), that interferes with any party’s uninterrupted use
-                enjoyment of the Site or modifies, impairs, disrupts, alters, or interferes with
-                use, features, functions, operation, or maintenance of the Site.
-              </li>
-              <li>
-                Engage in any automated use of the system, such as using scripts to send comments
-                using any data mining, robots, or similar data gathering and extraction tools.
-              </li>
-              <li>Delete the copyright or other proprietary rights notice from any Content.</li>
-              <li>
-                Attempt to impersonate another user or person or use the username of another user.
-              </li>
-              <li>Sell or otherwise transfer your profile. </li>
-              <li>
-                Upload or transmit (or attempt to upload or to transmit) any material that acts as a
-                passive or active information collection or transmission mechanism, including
-                without limitation, clear graphics interchange formats (&quot;gifs&quot;), 1×1
-                pixels, web bugs, cookies, or other similar devices (sometimes referred to as
-                &quot;spyware&quot; or &quot;passive collection mechanisms&quot; or
-                &quot;pcms&quot;).
-              </li>
-              <li>
-                Interfere with, disrupt, or create an undue burden on the Site or the networks or
-                services connected to the Site. Site.
-              </li>
-              <li>
-                Harass, annoy, intimidate, or threaten any of our employees or agents engaged in
-                engaged in providing any portion of the Site to you.
-              </li>
-              <li>
-                Attempt to bypass any measures of the Site designed to prevent or restrict access to
-                the Site, or any portion of the Site.
-              </li>
-              <li>
-                Copy or adapt the Site’s software, including but not limited to Flash, PHP, HTML,
-                JavaScript, or other code.
-              </li>
-              <li>
-                Decipher, decompile, disassemble, or reverse engineer any of the software comprising
-                or in any way making up a part of the Site.
-              </li>
-              <li>
-                Except as may be the result of standard search engine or Internet browser usage,
-                use, launch, develop, or distribute any automated system, limitation, any spider,
-                robot, cheat utility, scraper, or offline reader that reader that accesses the Site,
-                or using or launching any unauthorized script or other software.
-              </li>
-              <li>Use a buying agent or purchasing agent to make purchases on the Site.</li>
-              <li>
-                Make any unauthorized use of the Site, including collecting usernames and/or email
-                and/or email addresses of users by electronic or other means for the purpose of
-                sending unsolicited email, or creating user accounts by automated means or under
-                false pretenses.
-              </li>
-              <li>
-                Use the Site as part of any effort to compete with us or otherwise use the Site
-                and/or the Content for any revenue-generating endeavor or commercial enterprise.
-              </li>
-            </ol>
-          </Typography>
+          <ol>
+            <li>
+              Systematically retrieve data or other content from the Site to create or compile,
+              directly or indirectly, a collection, compilation, database, or directory without
+              written permission from us.
+            </li>
+            <li>
+              Trick, defraud, or mislead us and other users, especially in any attempt to learn
+              sensitive account information such as user passwords.
+            </li>
+            <li>
+              Circumvent, disable, or otherwise interfere with security-related Site, including
+              features that prevent or restrict the use or copying of any Content or enforce
+              limitations on the use of the Site and/or the Content contained therein.
+            </li>
+            <li>Disparage, tarnish, or otherwise harm, in our opinion, us and/or the Site.</li>
+            <li>
+              Use any information obtained from the Site in order to harass, abuse, or harm person.
+            </li>
+            <li>
+              Make improper use of our support services or submit false reports of abuse or abuse or
+              misconduct.
+            </li>
+            <li>Use the Site in a manner inconsistent with any applicable laws or regulations.</li>
+            <li>Use the Site to advertise or offer to sell goods and services.</li>
+            <li>Engage in unauthorized framing of or linking to the Site.</li>
+            <li>
+              Upload or transmit (or attempt to upload or to transmit) viruses, Trojan horses, or
+              other material, including excessive use of capital letters and spamming posting
+              posting of repetitive text), that interferes with any party’s uninterrupted use
+              enjoyment of the Site or modifies, impairs, disrupts, alters, or interferes with use,
+              features, functions, operation, or maintenance of the Site.
+            </li>
+            <li>
+              Engage in any automated use of the system, such as using scripts to send comments
+              using any data mining, robots, or similar data gathering and extraction tools.
+            </li>
+            <li>Delete the copyright or other proprietary rights notice from any Content.</li>
+            <li>
+              Attempt to impersonate another user or person or use the username of another user.
+            </li>
+            <li>Sell or otherwise transfer your profile. </li>
+            <li>
+              Upload or transmit (or attempt to upload or to transmit) any material that acts as a
+              passive or active information collection or transmission mechanism, including without
+              limitation, clear graphics interchange formats (&quot;gifs&quot;), 1×1 pixels, web
+              bugs, cookies, or other similar devices (sometimes referred to as &quot;spyware&quot;
+              or &quot;passive collection mechanisms&quot; or &quot;pcms&quot;).
+            </li>
+            <li>
+              Interfere with, disrupt, or create an undue burden on the Site or the networks or
+              services connected to the Site. Site.
+            </li>
+            <li>
+              Harass, annoy, intimidate, or threaten any of our employees or agents engaged in
+              engaged in providing any portion of the Site to you.
+            </li>
+            <li>
+              Attempt to bypass any measures of the Site designed to prevent or restrict access to
+              the Site, or any portion of the Site.
+            </li>
+            <li>
+              Copy or adapt the Site’s software, including but not limited to Flash, PHP, HTML,
+              JavaScript, or other code.
+            </li>
+            <li>
+              Decipher, decompile, disassemble, or reverse engineer any of the software comprising
+              or in any way making up a part of the Site.
+            </li>
+            <li>
+              Except as may be the result of standard search engine or Internet browser usage, use,
+              launch, develop, or distribute any automated system, limitation, any spider, robot,
+              cheat utility, scraper, or offline reader that reader that accesses the Site, or using
+              or launching any unauthorized script or other software.
+            </li>
+            <li>Use a buying agent or purchasing agent to make purchases on the Site.</li>
+            <li>
+              Make any unauthorized use of the Site, including collecting usernames and/or email
+              and/or email addresses of users by electronic or other means for the purpose of
+              sending unsolicited email, or creating user accounts by automated means or under false
+              pretenses.
+            </li>
+            <li>
+              Use the Site as part of any effort to compete with us or otherwise use the Site and/or
+              the Content for any revenue-generating endeavor or commercial enterprise.
+            </li>
+          </ol>
           <Typography variant="subtitle2" paragraph>
             USER GENERATED CONTRIBUTIONS
           </Typography>
