b18cfb0225cb660a181f2889eb00169a71e001ef
ovation22 <john@1north.com>
2020-08-05 22:05:04 -0400

    John, Clayton, Jordan playing with React Testing Library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
+import mediaQuery from 'css-mediaquery';
 import App from './App';
 
-test('renders Hooray!', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hooray!/i);
-  expect(linkElement).toBeInTheDocument();
+function createMatchMedia(width) {
+  return query => ({
+    matches: mediaQuery.match(query, { width }),
+    addListener: () => {},
+    removeListener: () => {},
+  });
+}
+
+describe('App tests', () => {
+  beforeAll(() => {
+    window.matchMedia = createMatchMedia('1200px');
+  });
+
+  test('renders App Menu!', () => {
+    const { getByAltText, getByRole } = render(<App />);
+    expect(getByAltText('logo-main')).toBeInTheDocument();
+    expect(getByAltText('logo-speaker')).toBeInTheDocument();
+
+    const menu = getByRole('menu');
+    expect(within(menu).getByText(/home/i)).toBeInTheDocument();
+    expect(within(menu).getByText(/speakers/i)).toBeInTheDocument();
+    expect(within(menu).getByText(/communities/i)).toBeInTheDocument();
+    expect(within(menu).getByText(/conferences/i)).toBeInTheDocument();
+
+    // const searchForm = getByRole('search');
+
+    // searchForm.submit();
+
+    // const searchBox = within(menu).getByPlaceholderText('');
+    // expect(searchBox).toBeInTheDocument();
+    // fireEvent.submit(searchBox, { target: searchForm });
+
+    // fireEvent.submit(searchForm, { target: { value: '' }});
+    // fireEvent.keyDown(searchField, { key: 'enter', keyCode: 13 })
+    // userEvent.type(searchBox, '{enter}');
+  });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+window.scrollTo = (x) => x
+
+global.matchMedia = media => ({
+    addListener: () => {},
+    removeListener: () => {},
+    matches: media === '(min-width: 1200px)',
+});
