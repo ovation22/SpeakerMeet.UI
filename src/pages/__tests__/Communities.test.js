d16867aed13f3df321c184e30ce3971d4ba3d098
Aaron Ward <Aaron@Comcept.net>
2020-08-29 23:08:12 -0400

    tested for expected fields to be rendered

--- src/pages/__tests__/Communities.test.js
+++ src/pages/__tests__/Communities.test.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { act } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import { renderWithRouter } from '../../utils/test.utilitiy';
 import Communities from '../Communities';
 
 describe('Communities', () => {
-  it('should show communities', async () => {
+  it('should render expected fields from list of returned communities', async () => {
     // arrange
     const communities = [
       {
-        id: 'idValue',
-        name: 'nameValue',
-        slug: 'slugValue',
-        location: 'locationValue',
-        description: 'descriptionValue',
+        id: 'idValue1',
+        name: 'nameValue1',
+        slug: 'slug-value-1',
+        location: 'locationValue1',
+        description: 'descriptionValue1',
+        paginationInfo: {
+          totalItems: 9,
+          itemsPerPage: 9,
+          actualPage: 0,
+          totalPages: 1,
+          previous: 'is-disabled',
+          next: 'is-disabled',
+        },
+      },
+      {
+        id: 'idValue2',
+        name: 'nameValue2',
+        slug: 'slug-value-2',
+        location: 'locationValue2',
+        description: 'descriptionValue2',
+        paginationInfo: {
+          totalItems: 9,
+          itemsPerPage: 9,
+          actualPage: 0,
+          totalPages: 1,
+          previous: 'is-disabled',
+          next: 'is-disabled',
+        },
       },
     ];
     const mockJsonPromise = Promise.resolve(communities);
@@ -27,5 +50,12 @@ describe('Communities', () => {
     await act(async () => renderWithRouter(tree));
 
     // assert
+    screen.getByText('Find a Community');
+
+    communities.forEach(community => {
+      screen.getByText(community.name);
+      screen.getByText(community.location);
+      screen.getByText(community.description);
+    });
   });
 });
