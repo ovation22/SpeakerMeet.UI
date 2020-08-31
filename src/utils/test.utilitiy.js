b7739cee6373f5737cf30c1aa3295626e6300351
Aaron Ward <Aaron@Comcept.net>
2020-08-31 16:07:06 -0400

    disabled no-extraneous-dependencies

--- src/utils/test.utilitiy.js
+++ src/utils/test.utilitiy.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { createMemoryHistory } from 'history';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { render } from '@testing-library/react';
