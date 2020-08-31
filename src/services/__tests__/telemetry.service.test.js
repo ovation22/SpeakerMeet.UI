82393609e51b959ca3939bc40cf683ba7c19c596
FreakinWard <aaron.l.ward@gmail.com>
2020-08-31 19:44:07 -0400

    Added Test Coverage for Communities & telemetry.service (#72)
    * added test coverage for telemetry.service.test.js
    
    * added test coverage scripts
    
    * cleaned up test naming
    
    * rendering Communities.js within a test
    
    * tested for expected fields to be rendered
    
    * tested for a failed community request
    
    * Merge branch 'master' into more-tests
    
    # Conflicts:
    #	package-lock.json
    #	package.json
    
    * re-added .all-contributersrc
    
    * renamed to .all-contributorsrc
    
    * moved history to dev dependencies
    
    * reimplemented await for fetchData
    
    * disabled no-extraneous-dependencies
    
    * disabled no-extraneous-dependencies for entire file
    
    Co-authored-by: Aaron Ward <Aaron@Comcept.net>

---
+++ src/services/__tests__/telemetry.service.test.js
@@ -0,0 +1,81 @@
+import * as ApplicationInsightsReact from '@microsoft/applicationinsights-react-js';
+import * as ApplicationInsightsWeb from '@microsoft/applicationinsights-web';
+import * as telemetryService from '../telemetry.service';
+
+describe('telemetry.service', () => {
+  const mockApplicationInsights = {
+    loadAppInsights: jest.fn(),
+  };
+
+  beforeEach(() => {
+    jest
+      .spyOn(ApplicationInsightsWeb, 'ApplicationInsights')
+      .mockImplementationOnce(() => mockApplicationInsights);
+  });
+
+  describe('applicationInsights', () => {
+    it('should expose ai as a function', () => {
+      // arrange
+      // act
+      const result = telemetryService.applicationInsights;
+
+      // assert
+      expect(result).toHaveProperty('appInsights');
+      expect(result).toHaveProperty('initialize');
+      expect(result).toHaveProperty('reactPlugin');
+    });
+
+    it('should throw an error given browserHistory is null ', () => {
+      // arrange
+      const instrumentationKey = 'instrumentationKey';
+      const history = null;
+      const expectedError = new Error('Could not initialize Telemetry Service');
+
+      // act
+      try {
+        telemetryService.applicationInsights.initialize(instrumentationKey, history);
+      } catch (error) {
+        // assert
+        expect(error).toEqual(expectedError);
+      }
+    });
+
+    it('should throw an error given instrumentationKey is null ', () => {
+      // arrange
+      const instrumentationKey = null;
+      const history = {};
+      const expectedError = new Error('Could not identify instrumentation key');
+
+      // act
+      try {
+        telemetryService.applicationInsights.initialize(instrumentationKey, history);
+      } catch (error) {
+        // assert
+        expect(error).toEqual(expectedError);
+      }
+    });
+
+    it('should initialize without throwing an error', () => {
+      // arrange
+      const instrumentationKey = 'instrumentationKey';
+      const history = { listen: jest.fn() };
+
+      jest.spyOn(ApplicationInsightsReact, 'ReactPlugin');
+
+      // act
+      // assert
+      telemetryService.applicationInsights.initialize(instrumentationKey, history);
+    });
+  });
+
+  describe('getAppInsights', () => {
+    it('should return appInsights', () => {
+      // arrange
+      // act
+      const result = telemetryService.getAppInsights();
+
+      // assert
+      expect(result).toBe(mockApplicationInsights);
+    });
+  });
+});
---
+++ src/utils/test.utilitiy.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router';
+
+// eslint-disable-next-line import/prefer-default-export
+export function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  // eslint-disable-next-line react/prop-types
+  const Wrapper = ({ children }) => <Router history={history}>{children}</Router>;
+  return {
+    ...render(ui, { wrapper: Wrapper }),
+    // adding `history` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    history,
+  };
+}
