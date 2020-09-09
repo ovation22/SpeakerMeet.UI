aa64a84eb4acd1a4044d4c532cf1d8aeb983386d
Aaron Ward <Aaron@Comcept.net>
2020-09-08 23:10:51 -0400

    removed unnecessary fragment

--- src/components/SpeakerCard.js
+++ src/components/SpeakerCard.js
@@ -43,40 +43,38 @@ export default function SpeakerCard({ post, ...rest }) {
   const altImage = `${process.env.PUBLIC_URL}/images/placeholder.png`;
 
   return (
-    <>
-      <Card className={classes.card} {...rest}>
-        <CardActionArea>
-          <CardMedia
-            src={`${config.images}/${post.slug}.png`}
-            component="img"
-            title={post.name}
-            onError={e => {
-              e.target.onerror = null;
-              e.target.src = altImage;
-            }}
-          />
-          <CardContent>
-            <Typography component="h2" variant="h5" className={classes.cardTitle}>
-              {post.name}
-            </Typography>
-            <Typography variant="subtitle1" className={classes.cardLocation}>
-              <LocationOn className={classes.cardLocationIcon} />
-              {post.location}
-            </Typography>
-            <Typography variant="subtitle1" paragraph>
-              {post.description}
-            </Typography>
-            <div className={classes.container}>
-              <SocialLinks socialPlatforms={post.socialPlatforms} />
-            </div>
-            <div className={classes.container}>
-              <Tags tags={post.tags} />
-            </div>
-          </CardContent>
-        </CardActionArea>
-        <CardActions />
-      </Card>
-    </>
+    <Card className={classes.card} {...rest}>
+      <CardActionArea>
+        <CardMedia
+          src={`${config.images}/${post.slug}.png`}
+          component="img"
+          title={post.name}
+          onError={e => {
+            e.target.onerror = null;
+            e.target.src = altImage;
+          }}
+        />
+        <CardContent>
+          <Typography component="h2" variant="h5" className={classes.cardTitle}>
+            {post.name}
+          </Typography>
+          <Typography variant="subtitle1" className={classes.cardLocation}>
+            <LocationOn className={classes.cardLocationIcon} />
+            {post.location}
+          </Typography>
+          <Typography variant="subtitle1" paragraph>
+            {post.description}
+          </Typography>
+          <div className={classes.container}>
+            <SocialLinks socialPlatforms={post.socialPlatforms} />
+          </div>
+          <div className={classes.container}>
+            <Tags tags={post.tags} />
+          </div>
+        </CardContent>
+      </CardActionArea>
+      <CardActions />
+    </Card>
   );
 }
 
