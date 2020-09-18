61dd69e18c5cf0f6354a9ef74f206a7da34f40f2
Aaron Ward <Aaron@Comcept.net>
2020-09-18 19:54:21 -0400

    cleanup

--- src/hooks/useSpeakers.js
+++ src/hooks/useSpeakers.js
@@ -3,14 +3,6 @@ import endpoints from '../constants/endpoints';
 import routes from '../constants/routes';
 import { trackException } from '../services/telemetry.service';
 
-// function useSpeakers() {
-//   return useQuery('speakers', () => fetch(endpoints.speakersFeatured).then(res => res.json()));
-// }
-
-// const refetch = useQuery('speakers', () =>
-//   fetch(endpoints.speakersFeatured).then(res => res.json()),
-// ).then(data => setSpeakers(data));
-
 function useSpeakers() {
   const [error, setError] = useState(null);
   const [isLoaded, setLoaded] = useState(false);
