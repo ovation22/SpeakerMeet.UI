1e0a2dcd17c6d0015d5a8a62681e40d7b84ba4d1
Aaron Ward <Aaron@Comcept.net>
2020-09-28 21:52:18 -0400

    covered useConferencesFeatured.test.js

---
+++ src/components/__tests__/useConferencesFeatured.test.js
@@ -0,0 +1,49 @@
+import { renderHook } from '@testing-library/react-hooks';
+import useConferencesFeatured from '../../hooks/useConferencesFeatured';
+import routes from '../../constants/routes';
+
+describe('useConferencesFeatured', () => {
+  it('should should behave correctly given request succeeds', async () => {
+    // arrange
+    const conferencesResult = [{ path: 'pathValue' }];
+    const mockJsonPromise = Promise.resolve(conferencesResult);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    // act
+    const { result, waitForNextUpdate } = renderHook(() => useConferencesFeatured());
+
+    const expectedConferences = [
+      { ...conferencesResult[0], path: `${routes.conferences.path}/${conferencesResult[0].slug}` },
+    ];
+    // assert
+    expect(result.current.conferences).toEqual([]);
+    expect(result.current.isLoaded).toBe(false);
+
+    await waitForNextUpdate();
+
+    expect(result.current.conferences).toEqual(expectedConferences);
+    expect(result.current.isLoaded).toBe(true);
+  });
+
+  it('should behave correctly given request fails', async () => {
+    // arrange
+    const error = 'errorValue';
+    jest.spyOn(global, 'fetch').mockRejectedValue(error);
+
+    // act
+    const { result, waitForNextUpdate } = renderHook(() => useConferencesFeatured());
+
+    // assert
+    expect(result.current.conferences).toEqual([]);
+    expect(result.current.isLoaded).toBe(false);
+
+    await waitForNextUpdate();
+
+    expect(result.current.conferences).toEqual([]);
+    expect(result.current.isLoaded).toBe(true);
+    expect(result.current.error).toEqual(error);
+  });
+});
