f810144a4ac1458b1ddcfba041f9b2494dc0f7b3
Aaron Ward <Aaron@Comcept.net>
2020-10-03 23:21:02 -0400

    promise to await for useCommunitiesFeatured.js

--- src/hooks/useCommunitiesFeatured.js
+++ src/hooks/useCommunitiesFeatured.js
@@ -8,23 +8,24 @@ export default function useCommunitiesFeatured() {
   const [isLoaded, setLoaded] = useState(false);
   const [communities, setCommunities] = useState([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(endpoints.communitiesFeatured);
+      const data = await response.json();
+      const result = data.map(x => ({
+        ...x,
+        path: `${routes.communities.path}/${x.slug}`,
+      }));
+      setCommunities(result);
+      setLoaded(true);
+    } catch (e) {
+      setError(e);
+      setLoaded(true);
+      trackException(e);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(endpoints.communitiesFeatured);
-        const data = await response.json();
-        const result = data.map(x => ({
-          ...x,
-          path: `${routes.communities.path}/${x.slug}`,
-        }));
-        setCommunities(result);
-        setLoaded(true);
-      } catch (e) {
-        setError(e);
-        setLoaded(true);
-        trackException(e);
-      }
-    };
     fetchData();
   }, []);
 
