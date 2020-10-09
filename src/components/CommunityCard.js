7764da2ee8bf19af82d86a637b6cb57a97b19fb6
John Callaway <john@1north.com>
2020-10-08 21:22:08 -0400

    Community Detail

---
+++ src/components/CommunityCard.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
+import LocationOn from '@material-ui/icons/LocationOn';
+import config from '../constants/config';
+import SocialLinks from './SocialLinks';
+import Tags from './Tags';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+    textAlign: 'left',
+  },
+  cardTitle: {
+    textAlign: 'center',
+  },
+  cardLocation: {
+    height: 20,
+    verticalAlign: 'middle',
+    margin: theme.spacing(2, 0),
+  },
+  cardLocationIcon: {
+    height: 20,
+    verticalAlign: 'middle',
+  },
+  cardViewProfile: {
+    marginLeft: 'auto',
+  },
+  container: {
+    float: 'left',
+    width: '100%',
+  },
+}));
+
+export default function CommunityCard({ post, ...rest }) {
+  const classes = useStyles();
+  const altImage = `${process.env.PUBLIC_URL}/images/placeholder.png`;
+
+  return (
+    <Card className={classes.card} {...rest}>
+      <CardActionArea>
+        <CardMedia
+          src={`${config.images}/${post.slug}.png`}
+          component="img"
+          title={post.name}
+          onError={e => {
+            e.target.onerror = null;
+            e.target.src = altImage;
+          }}
+        />
+        <CardContent>
+          <Typography component="h2" variant="h5" className={classes.cardTitle}>
+            {post.name}
+          </Typography>
+          <Typography variant="subtitle1" className={classes.cardLocation}>
+            <LocationOn className={classes.cardLocationIcon} />
+            {post.location}
+          </Typography>
+          <Typography variant="subtitle1" paragraph>
+            {post.description}
+          </Typography>
+          <div className={classes.container}>
+            <SocialLinks socialPlatforms={post.socialPlatforms} />
+          </div>
+          <div className={classes.container}>
+            <Tags tags={post.tags} />
+          </div>
+        </CardContent>
+      </CardActionArea>
+      <CardActions />
+    </Card>
+  );
+}
+
+CommunityCard.defaultProps = {
+  post: null,
+};
+
+CommunityCard.propTypes = {
+  post: PropTypes.shape({
+    name: PropTypes.string,
+    slug: PropTypes.string,
+    location: PropTypes.string,
+    description: PropTypes.string,
+    path: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string),
+    socialPlatforms: PropTypes.arrayOf(
+      PropTypes.shape({ name: PropTypes.string, url: PropTypes.string }),
+    ),
+  }),
+};
---
+++ src/pages/__tests__/CommunityDetail.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { act, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import CommunityDetail from '../CommunityDetail';
+import { render } from '../../utils/test.utilitiy';
+
+describe('CommunityDetail', () => {
+  it('should render the expected Community fields', async () => {
+    // arrange
+    const community = {
+      id: 'idValue',
+      name: 'nameValue1',
+      slug: 'slugValue1',
+      location: 'locationValue1',
+      description: 'descriptionValue1',
+      path: 'pathValue1',
+      tags: ['tag1', 'tag2'],
+      socialPlatforms: [{ name: 'platform', url: 'platformUrl' }],
+    };
+
+    const communityResponseMock = Promise.resolve({
+      json: () => Promise.resolve(community),
+    });
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => communityResponseMock);
+
+    const communitiesFeatured = [];
+    const communitiesFeaturedResponseMock = Promise.resolve({
+      json: () => Promise.resolve(communitiesFeatured),
+    });
+    jest.spyOn(global, 'fetch').mockImplementationOnce(() => communitiesFeaturedResponseMock);
+
+    const theme = createMuiTheme();
+
+    const tree = (
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <CommunityDetail />
+        </BrowserRouter>
+      </ThemeProvider>
+    );
+
+    // act
+    await act(async () => render(tree));
+
+    // assert
+    screen.getByText(community.name);
+    screen.getByText(community.location);
+    screen.getByText(community.description);
+
+    community.socialPlatforms.forEach(platform => {
+      expect(screen.getByLabelText(platform.name)).toHaveAttribute('href', platform.name.url);
+    });
+
+    community.tags.forEach(tag => screen.getByText(tag));
+  });
+});
