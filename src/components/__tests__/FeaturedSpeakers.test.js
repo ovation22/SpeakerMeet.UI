abc916d5f46a179285b5d42191a63e11224fcad1
John Callaway <john@1north.com>
2020-10-08 21:57:23 -0400

    Featured Speakers

---
+++ src/components/__tests__/FeaturedSpeakers.test.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import FeaturedSpeakers from '../FeaturedSpeakers';
+import * as useSpeakersFeatured from '../../hooks/useSpeakersFeatured';
+
+describe('FeaturedSpeakers', () => {
+  it('should notify user of loading', () => {
+    // arrange
+    const useSpeakersFeaturedMock = () => {
+      return {
+        error: null,
+        isLoaded: false,
+        speakers: [],
+      };
+    };
+    jest.spyOn(useSpeakersFeatured, 'default').mockImplementationOnce(useSpeakersFeaturedMock);
+
+    const tree = (
+      <BrowserRouter>
+        <FeaturedSpeakers />
+      </BrowserRouter>
+    );
+
+    // act
+    const { getByTestId } = render(tree);
+
+    // assert
+    getByTestId('loading');
+  });
+
+  it('should render expected speaker fields', () => {
+    // arrange
+    const speaker = {
+      id: 'idValue',
+      name: 'nameValue',
+      description: 'descriptionValue',
+      location: 'locationValue',
+      path: 'pathValue',
+      slug: 'slugValue',
+    };
+    const speakers = [speaker];
+
+    const useSpeakersFeaturedMock = () => {
+      return {
+        error: null,
+        isLoaded: true,
+        speakers,
+      };
+    };
+    jest.spyOn(useSpeakersFeatured, 'default').mockImplementationOnce(useSpeakersFeaturedMock);
+
+    const tree = (
+      <BrowserRouter>
+        <FeaturedSpeakers />
+      </BrowserRouter>
+    );
+
+    // act
+    const { getByText } = render(tree);
+
+    // assert
+    getByText(speaker.name);
+  });
+
+  it('should alert of error', () => {
+    // arrange
+    const error = new Error('error message');
+
+    const useSpeakersFeaturedMock = () => {
+      return {
+        error,
+        isLoaded: true,
+        speakers: [],
+      };
+    };
+    jest.spyOn(useSpeakersFeatured, 'default').mockImplementationOnce(useSpeakersFeaturedMock);
+
+    const tree = (
+      <BrowserRouter>
+        <FeaturedSpeakers />
+      </BrowserRouter>
+    );
+
+    // act
+    const { getByTestId } = render(tree);
+
+    // assert
+    expect(getByTestId('snackError')).toContainHTML(error.message);
+  });
+});
