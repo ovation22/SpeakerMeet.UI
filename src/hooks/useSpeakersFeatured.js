cfa697cdbd2bb23a1cf8dfa51055e91fc74efe8f
John Callaway <ovation22@users.noreply.github.com>
2020-10-08 22:00:51 -0400

    Featured Speakers (#94)

---
+++ src/hooks/useSpeakersFeatured.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import endpoints from '../constants/endpoints';
+import routes from '../constants/routes';
+import { trackException } from '../services/telemetry.service';
+
+export default function useSpeakersFeatured() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const [speakers, setSpeakers] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(endpoints.speakersFeatured);
+      const data = await response.json();
+      const result = data.map(x => ({
+        ...x,
+        path: `${routes.speakers.path}/${x.slug}`,
+      }));
+      setSpeakers(result);
+    } catch (e) {
+      setError(e);
+      trackException(e);
+    }
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return {
+    error,
+    isLoaded,
+    speakers,
+  };
+}
