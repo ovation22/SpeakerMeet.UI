61f49ae884abdcdda2175bd6ceb6d9859ecbf57f
Aaron Ward <Aaron@Comcept.net>
2020-10-17 00:20:45 -0400

    rolled back paging

--- src/hooks/useSpeakers.js
+++ src/hooks/useSpeakers.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import endpoints from '../constants/endpoints';
 import routes from '../constants/routes';
 import { trackException } from '../services/telemetry.service';
@@ -7,47 +7,31 @@ export default function useSpeakers() {
   const [error, setError] = useState(null);
   const [isLoaded, setLoaded] = useState(false);
   const [speakers, setSpeakers] = useState([]);
-  const [pageIndex, setPageIndex] = useState(0);
-  const [pageSize] = useState(2); // TODO: how is page size set?
-
-  const fetchData = useCallback(async () => {
-    try {
-      const url = `${endpoints.speakers}?pageIndex=${pageIndex}&itemsPage=${pageSize}`;
-
-      const response = await fetch(url);
-      const data = await response.json();
-      const result = data.speakers.map(x => ({
-        ...x,
-        path: `${routes.speakers.path}/${x.slug}`,
-      }));
-      setSpeakers(result);
-    } catch (e) {
-      setError(e);
-      trackException(e);
-    }
-    setLoaded(true);
-  }, [pageIndex, pageSize]);
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = `${endpoints.speakers}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+        const result = data.speakers.map(x => ({
+          ...x,
+          path: `${routes.speakers.path}/${x.slug}`,
+        }));
+        setSpeakers(result);
+      } catch (e) {
+        setError(e);
+        trackException(e);
+      }
+      setLoaded(true);
+    };
     fetchData();
-  }, [fetchData, pageIndex, pageSize]);
-
-  const previousPage = useCallback(() => {
-    setPageIndex(pageIndex - 1);
-  }, [pageIndex]);
-
-  const nextPage = useCallback(() => {
-    setPageIndex(pageIndex + 1);
-  }, [pageIndex]);
-
-  const isFirstPage = useMemo(() => pageIndex === 0, [pageIndex]);
+  }, []);
 
   return {
     speakers,
     error,
     isLoaded,
-    previousPage,
-    nextPage,
-    isFirstPage,
   };
 }
--- src/pages/Speakers.js
+++ src/pages/Speakers.js
@@ -2,15 +2,13 @@ import { CircularProgress } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import { Helmet } from 'react-helmet-async';
 import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import { NavigateNext, NavigateBefore } from '@material-ui/icons';
 import ErrorSnackbar from '../components/ErrorSnackbar';
 import FindABanner from '../components/FindABanner';
 import ResultList from '../components/ResultList';
 import useSpeakers from '../hooks/useSpeakers';
 
 export default function Speakers() {
-  const { speakers, error, isLoaded, previousPage, nextPage, isFirstPage } = useSpeakers();
+  const { speakers, error, isLoaded } = useSpeakers();
 
   return (
     <>
@@ -21,19 +19,7 @@ export default function Speakers() {
       <FindABanner text="Speaker" />
 
       <Container maxWidth="lg" style={{ padding: 24, minHeight: '100vh' }}>
-        {!isLoaded ? (
-          <CircularProgress data-testid="loading" />
-        ) : (
-          <>
-            <ResultList data={speakers} />
-            <IconButton aria-label="previous page" onClick={previousPage} disabled={isFirstPage}>
-              <NavigateBefore />
-            </IconButton>
-            <IconButton aria-label="next page" onClick={nextPage}>
-              <NavigateNext />
-            </IconButton>
-          </>
-        )}
+        {!isLoaded ? <CircularProgress data-testid="loading" /> : <ResultList data={speakers} />}
       </Container>
 
       <ErrorSnackbar error={error} />
