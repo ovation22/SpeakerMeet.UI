085e282a3f2c39bcdb6b77340a8cb629c587f723
John Callaway <ovation22@users.noreply.github.com>
2021-04-07 20:30:26 -0400

    SonarQube (#118)

---
+++ sonar-project.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import sonarqubeScanner from 'sonarqube-scanner';
+
+sonarqubeScanner(
+  {
+    serverUrl: '#{sonarQubeServerUrl}#',
+    token: '#{sonarQubeToken}#',
+    options: {
+      'sonar.projectName': '#{projectName}#',
+      'sonar.projectKey': '#{projectKey}#',
+      'sonar.sources': './src',
+      'sonar.exclusions': '**/*.test.*,**/__snapshots__/**,src/*.ts',
+      'sonar.tests': './src',
+      'sonar.test.inclusions': './src/**/*.test.tsx',
+      'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
+      'sonar.testExecutionReportPaths': 'reports/test-report.xml',
+    },
+  },
+  () => process.exit(),
+);
